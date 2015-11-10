68eb356ba5864adc4a2f9eb261463936e4004275
Siegfried Kiermayer <sicaine@gmail.com>
2015-11-10 23:22:42 +0100

    add basic stdin input for basic template
    Trying to play around with git env under windows and trying to find out a
    good basic schema for calling those challenges externaly.

--- Bubble_Sort/java/bubblesort.java
+++ Bubble_Sort/java/bubblesort.java
@@ -1,6 +1,21 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 class Bubblesort {
 
+	/**
+	* @throws IOException
+	*/
 	public static void main(String[] args) {
-		System.out.println("test");
+		try{
+			InputStreamReader stdin = new InputStreamReader(System.in);
+			int in = 0;
+			while( (in = stdin.read()) != -1) {
+				System.out.println(in);
+			}
+			
+			stdin.close();
+		}catch(Exception e){
+		}
 	}
 }
